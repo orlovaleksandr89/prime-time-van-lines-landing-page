44952f40b79b4437afe2029871a1b3c678361059
Oleksandr orlov <orlovaleksandr89@gmail.com>
2021-11-03 19:23:44 -0700

    mobile version fix + smooth scroll

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,4 @@
-$('wrapper').addClass('loaded')
-
-$('.icon-menu').click(function (event) {
-  $(this).toggleClass('active')
-  $('.menu__body').toggleClass('active')
-  $('body').toggleClass('lock')
-})
+// $('wrapper').addClass('loaded')
 
 function ibg() {
   $.each($('.ibg'), function (index, val) {
@@ -17,18 +11,64 @@ function ibg() {
   })
 }
 ibg()
+
+//burger meny toggle
+const iconMenu = document.querySelector('.icon-menu')
 const menuBody = document.querySelector('.menu__body')
 const links = document.querySelectorAll('.menu__link')
+
+iconMenu.addEventListener('click', () => {
+  iconMenu.classList.toggle('active')
+  menuBody.classList.toggle('active')
+  document.body.classList.toggle('lock')
+})
+
 links.forEach((link) =>
   link.addEventListener('click', () => {
     if (menuBody.classList.contains('active')) {
-      console.log('click active')
       menuBody.classList.remove('active')
+      iconMenu.classList.remove('active')
+      document.body.classList.remove('lock')
     }
   })
 )
-// const callMe = document.querySelector('.callme')
-// setTimeout(() => {
-//   callMe.classList.add('active')
-//   console.log('ticktack')
-// }, 2000)
+
+// smooth scroll
+const linksAnchors = document.querySelectorAll('[href^="#"]')
+linksAnchors.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    let href = this.getAttribute('href').substring(1)
+
+    const scrollTarget = document.getElementById(href)
+    // const topOffset = document.querySelector('.scrollto').offsetHight
+    const topOffset = 0
+    const elementPosition = scrollTarget.getBoundingClientRect().top
+    const offsetPosition = elementPosition - topOffset
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth'
+    })
+  })
+})
+
+// scroll to top button
+
+const backToTopbtn = document.querySelector('#goToHome')
+
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 650 ||
+    document.documentElement.scrollTop > 650
+  ) {
+    backToTopbtn.style.display = 'block'
+  } else {
+    backToTopbtn.style.display = 'none'
+  }
+}
